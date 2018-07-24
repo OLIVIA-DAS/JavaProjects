1aecffcce776db7885141e7cfa60d3288ffed4d3
OLIVIA-DAS <OLIVIA-DAS@users.noreply.github.com>
2018-07-24 11:28:00 +0530

    MAMMALS-HUMAN-EYE CLASS

---
+++ JavaStuff2/src/com/deloitte/main/MainClass.java
@@ -0,0 +1,11 @@
+package com.deloitte.main;
+public class MainClass{
+	public static void main(String[] args) {
+		Man man1=new Man();
+		man1.leftEye.color="black";
+		man1.leftEye.power=2.5f;
+		System.out.println(man1.leftEye.color);
+		System.out.println(man1.leftEye.power);
+	}
+}
+		
---
+++ JavaStuff2/src/com/deloitte/main/Man.java
@@ -0,0 +1,7 @@
+package com.deloitte.main;
+
+import com.deloitte.livingbeings.humans;
+
+public class Man extends humans{
+
+}
