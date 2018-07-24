b5898a750192569eb9eb36ecb127c357e36c2f4b
OLIVIA-DAS <OLIVIA-DAS@users.noreply.github.com>
2018-07-24 11:56:03 +0530

    strings

---
+++ JustStrings/src/com/deloitte/strings/MainClass.java
@@ -0,0 +1,13 @@
+package com.deloitte.strings;
+
+public class MainClass {
+	public static void main(String[] args) {
+		String s1,s2;
+		
+		
+		s1=new String("hi");
+		s2=s1;
+ 
+		System.out.println(s1==s2);
+	}
+}
