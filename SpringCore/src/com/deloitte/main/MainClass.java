e17e6ea38d90b1e638aa4dbb18b9f4afd9356bf4
OLIVIA-DAS <OLIVIA-DAS@users.noreply.github.com>
2018-08-02 17:25:19 +0530

    SpringCore

--- SpringCore/src/com/deloitte/main/MainClass.java
+++ SpringCore/src/com/deloitte/main/MainClass.java
@@ -3,6 +3,15 @@
  */
 package com.deloitte.main;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.util.SystemPropertyUtils;
+
+import com.deloitte.pojos.HelloIndia;
+import com.deloitte.pojos.HelloWorld;
+import com.deloitte.pojos.Point;
+import com.deloitte.pojos.TextEditor;
+
 /**
  * @author Tva0091
  *
@@ -13,6 +22,32 @@ public class MainClass {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+		Point point1=(Point)context.getBean("pointBean");
+		System.out.println(point1);
+		Point point2=(Point)context.getBean("pointBean");
+		System.out.println(point2);
+		point2.setX(5);
+		point2.setY(10);
+		System.out.println(point1);
+		HelloWorld helloworld=(HelloWorld)context.getBean("helloWorld");
+		helloworld.getMessage1();
+		helloworld.getMessage2();
+		HelloIndia helloindia=(HelloIndia)context.getBean("helloIndia");
+		helloindia.getMessage1();
+		helloindia.getMessage2();
+		helloindia.getMessage3();
+		TextEditor te1=(TextEditor)context.getBean("textEditorCDI");
+		te1.spellCheck();
+		TextEditor te2=(TextEditor)context.getBean("textEditorSDI");
+		te2.spellCheck();
+		TextEditor te3=(TextEditor)context.getBean("textEditorAWbN");
+		te3.spellCheck();
+		TextEditor te4=(TextEditor)context.getBean("textEditorAWbT");
+		te4.spellCheck();
+		TextEditor te5=(TextEditor)context.getBean("textEditorAWbT");
+		te5.spellCheck();
+		te5.getName();
 		// TODO Auto-generated method stub
 
 	}
