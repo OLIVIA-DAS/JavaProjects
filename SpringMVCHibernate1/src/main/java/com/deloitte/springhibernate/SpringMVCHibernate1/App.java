e1222e1004fb06f4b0c4b35734ad302cad632e3f
OLIVIA-DAS <OLIVIA-DAS4@gmail.com>
2018-08-08 18:56:57 +0530

    SpringMVCHibernate

---
+++ SpringMVCHibernate1/src/main/java/com/deloitte/springhibernate/SpringMVCHibernate1/App.java
@@ -0,0 +1,13 @@
+package com.deloitte.springhibernate.SpringMVCHibernate1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ SpringMVCHibernate1/src/main/java/com/deloitte/springhibernate/service/PersonService.java
@@ -0,0 +1,17 @@
+package com.deloitte.springhibernate.service;
+
+import java.util.List;
+
+import com.deloitte.springhibernate.dao.PersonDAO;
+import com.deloitte.springhibernate.model.Person;
+
+
+public interface PersonService {
+
+	public void addPerson(Person p);
+	public void updatePerson(Person p);
+	public List<Person> listPersons();
+	public Person getPersonById(int id);
+	public void removePerson(int id);
+	
+}
